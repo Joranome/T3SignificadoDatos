9da8e182408cf2630acb7c663b3103da5458de8f
Joranome <43706625+Joranome@users.noreply.github.com>
2022-04-06 09:12:06 -0600

    Initial commit

---
+++ pyinstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Autor: Ricardo Quintero
+# Description : Shell script para instalar librerias para Jupyter Notebook
+# Activación: chmod +x pylibinstall.sh
+
+pip install pandas
+pip install statsmodels
